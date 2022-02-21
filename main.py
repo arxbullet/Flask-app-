777c53b58652c89e82ff2b2da29d303b81557020
arx_bullet <aranzav.c.b@gmail.com>
2022-02-21 20:28:18 -0300

    usar plantillas en flask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from django.shortcuts import redirect
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect, render_template
 #inicio 
 #1. instalar flask  : pip install Flask
 #2. crear archivo main en carpeta en donde instalamos Flask
@@ -11,28 +10,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): # el nombre de la ruta es el nombre de la funcion
-    return"aprendiendo flask"
+    return render_template('index.html')
 
 #parametro opcional
 @app.route('/informacion')
 #parametro obligatorio
 @app.route('/informacion/<string:nombre>')
 def informacion(nombre = 'default'):
-    return f"<h1>Hola, {nombre}:Informacion</h1>"
-
+    #return f"<h1>Hola, {nombre}:Informacion</h1>"
+    return render_template('informacion.html', nombre = nombre)
 
 @app.route('/contacto')
 @app.route('/contacto/<redireccion>')
 def contacto(redireccion = None):
-
     if redireccion is not None:
         return redirect(url_for('lenguajes'))
-
-    return "<h1>Contacto</h1>"
+   # return "<h1>Contacto</h1>"
+    return render_template('contacto.html')
 
 @app.route('/lenguajes')
 def lenguajes():
-    return "<h1>Lenguajes</h1>"
+
+    return render_template('lenguajes.html')
+    
+    #return "<h1>Lenguajes</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
