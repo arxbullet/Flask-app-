235be88d6cb266375b64ea4ecfb6ee299c303c63
arx_bullet <aranzav.c.b@gmail.com>
2022-02-23 16:54:02 -0300

    conectar sql y crear ruta para nuevos registros

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 from datetime import datetime
+from flask_mysqldb import MySQL 
 #inicio 
 #1. instalar flask  : pip install Flask
 #2. crear archivo main en carpeta en donde instalamos Flask
 #3. iniciar app con python main.py
 #instanciar app y frmawork para usarlo
 app = Flask(__name__)
+#Para usar my sql primero debo instalar pip install flask-mysqldb
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'proyectoflask'
+
+mysql = MySQL(app)
 
 @app.context_processor
 def date_now():
@@ -47,6 +56,18 @@ def lenguajes():
                             dato2='dato2')
     
     #return "<h1>Lenguajes</h1>"
+
+@app.route('/crear-coche')
+def crear_coche():
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor()
+        cursor.execute(f"INSERT INTO auto VALUES(null, 'Lambo', 'gallardo', 1000, 'los angeles')")
+        cursor.connection.commit()
+        return redirect(url_for('index'))
+
+    return render_template('crear_coche.html', 
+                            dato1='dato1',
+                            dato2='dato2')
 
 if __name__ == '__main__':
     app.run(debug=True)
